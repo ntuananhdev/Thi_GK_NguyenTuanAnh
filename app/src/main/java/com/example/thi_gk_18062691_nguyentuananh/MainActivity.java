40d22892675776e3449d1627dddcaf3ebe876fb0
Tuấn Anh <80771459+ntuananhdev@users.noreply.github.com>
2021-11-02 13:17:14 +0700

    add project

---
+++ app/src/main/java/com/example/thi_gk_18062691_nguyentuananh/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.thi_gk_18062691_nguyentuananh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
